0ee877a2edd49948928c60bfad883670619ecc00
christina-aj <christinajosefphine06@gmail.com>
2024-06-09 00:37:23 +0700

    laallaal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -153,6 +153,8 @@
                                     </div>
 
                                     </div> -->
+
+                                    {{-- lalalala --}}
                                     @foreach ($providers as $provider)
                                         <div class="col-md-6 col-lg-4 col-xl-3">
                                             <div class="rounded position-relative fruite-item">
