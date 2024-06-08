4172acc898de1b06f333c89a66802b4c630abd7c
christina-aj <christinajosefphine06@gmail.com>
2024-06-09 00:41:44 +0700

    commit akhir

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -153,9 +153,7 @@
                                     </div>
 
                                     </div> -->
-                                    {{-- lolololo --}}
-
-                                    {{-- lalalala --}}
+                                    
                                     @foreach ($providers as $provider)
                                         <div class="col-md-6 col-lg-4 col-xl-3">
                                             <div class="rounded position-relative fruite-item">
